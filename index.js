a5aad012dfd93c089605cc09f63d55817bc97f0b
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-05-18 16:29:15 -0400

    commit tree 2270217b713f02b9a31babfa9657832e7caadb4e

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution here
+const findMatching = (array , string) => array.filter(e=> e.toLowerCase() === string.toLowerCase() )
+const fuzzyMatch = (array , string) => array.filter(e=> e[0].toLowerCase() === string[0].toLowerCase() )
+
+const matchName = (arr , name) => arr.filter(e=> e.name === name)
